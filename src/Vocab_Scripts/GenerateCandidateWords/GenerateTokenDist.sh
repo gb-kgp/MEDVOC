b6e46ce32eaf1da81107dc225b2f5d32704a1533
Gunjan Balde <gunjanbalde@Gunjans-MacBook-Pro.local>
2024-05-15 18:19:08 +0530

    Updated CSV paths

--- src/Vocab_Scripts/GenerateCandidateWords/GenerateTokenDist.sh
+++ src/Vocab_Scripts/GenerateCandidateWords/GenerateTokenDist.sh
@@ -2,29 +2,7 @@
 
 python GenerateSubwordsBERT.py \
        -dataset EBM \
-       -path_pubmed /Users/gunjanbalde/Documents/SR-NG-MedVoc/SR-NG-MedVoc/MedicalDataset/CSVs_PubMed/Train_PubMed.csv  \
-       -path_data /Users/gunjanbalde/Documents/SR-NG-MedVoc/SR-NG-MedVoc/MedicalDataset/CSVs_EBM/Filtered_Train_BART.csv \
+       -path_pubmed ../../../data/Dataset-CSVs/PAC/Train.csv \
+       -path_data ../../../data/Dataset-CSVs/EBM/Train.csv \
        -csv_path ./TokenSplitDistribution-EBM_BERT.csv \
        -reference_column summary_text
-
-python GenerateSubwordsBERT.py \
-       -dataset BioASQ \
-       -path_pubmed /Users/gunjanbalde/Documents/SR-NG-MedVoc/SR-NG-MedVoc/MedicalDataset/CSVs_PubMed/Train_PubMed.csv  \
-       -path_data /Users/gunjanbalde/Documents/SR-NG-MedVoc/SR-NG-MedVoc/MedicalDataset/BioASQ_Bert/BioASQ_CSVs/Train.csv \
-       -csv_path ./TokenSplitDistribution-BioASQ_BERT.csv \
-       -reference_column target_text
-
-
-python GenerateSubwordsBERT.py \
-       -dataset MeQSum \
-       -path_pubmed /Users/gunjanbalde/Documents/SR-NG-MedVoc/SR-NG-MedVoc/MedicalDataset/CSVs_PubMed/Train_PubMed.csv  \
-       -path_data /Users/gunjanbalde/Documents/SR-NG-MedVoc/SR-NG-MedVoc/MedicalDataset/MeQSum/Train.csv \
-       -csv_path ./TokenSplitDistribution-MeQSum_BERT.csv \
-       -reference_column Summary
-
-python GenerateSubwordsBERT.py \
-         -dataset CHQ \
-         -path_pubmed /Users/gunjanbalde/Documents/SR-NG-MedVoc/SR-NG-MedVoc/MedicalDataset/CSVs_PubMed/Train_PubMed.csv  \
-         -path_data /Users/gunjanbalde/Documents/SR-NG-MedVoc/SR-NG-MedVoc/MedicalDataset/chq-summ-sd-rs/CHQ-CSVs/Train.csv \
-         -csv_path ./TokenSplitDistribution-CHQ_BERT.csv
-         -reference_column target_text
